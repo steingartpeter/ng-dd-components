152d236e6ee178d93aa3745c240788964ae5c43f
steingartpeter <steingart.p2@gmail.com>
2025-06-27 11:55:24 +0200

    Shared button vomponent added

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  title = input.required<string>();
+  icon = input.required<string>();
+}
