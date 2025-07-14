a1d24536cd98503f72ea9689104d1c65be347966
steingartpeter <steingart.p2@gmail.com>
2025-07-14 11:58:10 +0200

    Disable css ENCAPSULATION with Component setup

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation:ViewEncapsulation.None, // Disable encapsulation to extend css class
 })
 export class ControlComponent {
   //@Input({required:true}) title='';
