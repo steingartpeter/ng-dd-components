2bd73699615b4b739cada205f486399d702fc454
steingartpeter <steingart.p2@gmail.com>
2025-07-14 11:27:53 +0200

    NG-CONTEN / SELECT

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,13 +1,13 @@
 import { Component, input } from '@angular/core';
 
 @Component({
-  selector: 'button[appButton]',
+  selector: 'button[appButton], a[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-  title = input.required<string>();
-  icon = input.required<string>();
+  // title = input.required<string>();
+  // icon = input.required<string>();
 }
