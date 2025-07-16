c486f1e1e9828ed0c1c8632b37516e07cdf6f797
steingartpeter <steingart.p2@gmail.com>
2025-07-16 13:56:28 +0200

    @HostBinding, @HostListener

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,11 @@
-import { Component, input, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,10 +13,17 @@ import { Component, input, Input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation:ViewEncapsulation.None, // Disable encapsulation to extend css class
-  host:{class:'control'},
+  encapsulation: ViewEncapsulation.None, // Disable encapsulation to extend css class
+  host: { class: 'control', '(click)': 'OnClickFncIncmoponent_class()' },
+  // class<== possibly replaced by @HostBinding - not recommended b.c.
+  // click <== possibly replaced by @HostListener - same - officially not recommended
 })
 export class ControlComponent {
-  //@Input({required:true}) title='';
+  // @Input({required:true}) title='';
+  // @HostBinding('class') className = 'control';
+  @HostListener('click') onCLick(){/*Same placeholder*/}; 
   label = input.required<string>();
+  OnClickFncIncmoponent_class() {
+    // Just a placeholder
+  }
 }
