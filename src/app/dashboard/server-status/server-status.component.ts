7292f11f0b82afc21d57b2b7ce535e252df736e4
steingartpeter <steingart.p2@gmail.com>
2025-07-18 13:22:35 +0200

    LifeCycle hooks

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus:'online'|'offline'|'unknown' = 'online';
+
+  constructor(){
+    
+  }
+
+  ngOnInit(){
+    setInterval(()=>{
+      const rnd = Math.random(); // 0 - 0.99999999
+      if(rnd < 0.5){
+        this.currentStatus = 'online';
+      }else if(rnd < .9){
+        this.currentStatus = 'offline';
+      }else{
+        this.currentStatus = 'unknown';
+      }
+      console.log(`NEW STATE: ${this.currentStatus}`);
+    },5000);
+  }
 }
