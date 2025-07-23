5a59ff728fa6bb8280bb031bf22f124b763005b6
steingartpeter <steingart.p2@gmail.com>
2025-07-23 09:09:54 +0200

    @ViewChild/ElementRef<>

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -11,8 +11,12 @@ import { ControlComponent } from '../../../shared/control/control.component';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+
+  @ViewChild('form') form?:ElementRef<HTMLFormElement>;
+
   onSubmit(ttl: string, ticketText: string) {
     console.log(`ttl: ${ttl}`);
     console.log(`ticketText:${ticketText}`);
+    this.form?.nativeElement.reset();
   }
 }
